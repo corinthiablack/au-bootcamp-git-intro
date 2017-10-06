13e5121cd51c5920601eecb1e9786aaa333aa0de
Cori R. Black <crb0083@auburn.edu>
2017-10-06 15:40:35 -0500

    Cleaned up
    cleaned up code and notes. Ready for submit for grade.

--- count-fasta-seqs.sh
+++ count-fasta-seqs.sh
@@ -3,38 +3,17 @@
 #Script for Lab Assignment Four
 #The goal of this exercise is to work collaboratively with your team members to write a simple shell script 
 
-
-file="$1"
-
-#seperate the sequence from the sequence name
-name=`grep '>' $1`
-sequence=`grep -v '>' $1`
-
-#total sequence
-total=`echo $sequence | wc -m`
-
-#print output name and percent to file
-echo "$name" >${1}_sequencecount.txt
-echo "$total" >>${1}_sequencecount.txt
-
-
 #search for files included in the command line and print the total sequences and filename to the output
 for file in "$@"
 do
-	
         NUM=`grep '>' $file | wc -l`
        	filename=`basename $file`
 
-	echo $NUM $filename
-        
+	echo $NUM $filename        
 done
-
-#NEED TO FIGURE OUT HOW TO ADD TOTAL SEQUENCES TO PRINT AFTER THE LOOP
 
 #The final line should have the total number of sequences across all files
 TOTAL=`grep '>' $@ | wc -l`
 echo $TOTAL
 
-
 #working code should echo the name of the file without the full pathname and count the number of sequences within that file
-#probably not the right working code for this assignment, dont know why I got this version when I did my latest `git pull`
