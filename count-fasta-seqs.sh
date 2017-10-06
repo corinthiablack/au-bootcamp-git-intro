0beecda921f459179a84c780a810a93aeb640e95
Cori R. Black <crb0083@auburn.edu>
2017-10-06 12:06:06 -0500

    Cleaned up file
    Removed any non-working code and made it look nice so it is easy to read.

--- count-fasta-seqs.sh
+++ count-fasta-seqs.sh
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
+#!/bin/sh
 
-<<<<<<< HEAD
-#COMMENT HERE
 #Script for Lab Assignment Four
 #The goal of this exercise is to work collaboratively with your team members to write a simple shell script 
 
-#File represents the first argument 
-file="$1"
-=======
-=======
-#!/bin/sh
-
 #search for files included in the command line and print the total sequences and filename to the output
->>>>>>> 1e5e2baac1e01936c7fd5b5583ded0d7a3ed282f
-
 for file in "$@"
 do
 	
@@ -28,4 +18,3 @@ done
 #The final line should have the total number of sequences across all files
 TOTAL=`grep '>' $@ | wc -l`
 echo $TOTAL
->>>>>>> 2b489bedc7205c2208ad006e6c5b754d40d2efff
